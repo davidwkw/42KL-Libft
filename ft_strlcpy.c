2dc92eb7b5931d22379c9fbf4a41a80ef9f1dd5a
David Wang <devtesting404@gmail.com>
2021-09-12 22:37:55 +0800

    refactor strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -17,20 +17,18 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	src_len;
 	size_t	i;
 
-	if (dst && src)
+	if (!dst || !src)
+		return (0);
+	src_len = ft_strlen(src);
+	if (dstsize > 0)
 	{
-		src_len = ft_strlen(src);
-		if (dstsize > 0)
+		i = 0;
+		while (i < dstsize - 1 && src[i])
 		{
-			i = 0;
-			while (i < dstsize - 1 && src[i])
-			{
-				dst[i] = src[i];
-				i++;
-			}
-			dst[i] = '\0';
+			dst[i] = src[i];
+			i++;
 		}
-		return (src_len);
+		dst[i] = '\0';
 	}
-	return (0);
+	return (src_len);
 }
