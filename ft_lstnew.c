f6493d43f360dbff2b998eb1c16457a7df4b6a36
David Wang <devtesting404@gmail.com>
2024-01-12 22:16:17 +0800

    refactor: fix syntax

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -19,7 +19,7 @@ t_list	*ft_lstnew(void *content)
 	node = malloc(sizeof(t_list));
 	if (!node)
 		return (node);
-	(*node).content = content;
-	(*node).next = NULL;
+	node->content = content;
+	node->next = NULL;
 	return (node);
 }
