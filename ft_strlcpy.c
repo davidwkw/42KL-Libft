d1b537e964b8321066744feabb229d27a651ae27
David Wang <devtesting404@gmail.com>
2024-01-12 23:32:38 +0800

    refactor guard clause

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: kwang <kwang@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 11:07:25 by kwang             #+#    #+#             */
-/*   Updated: 2021/05/24 13:21:12 by kwang            ###   ########.fr       */
+/*   Updated: 2024/01/12 23:31:59 by kwang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	src_len = ft_strlen(src);
-	if (dstsize > 0)
+	if (dstsize <= 0)
+		return (src_len);
+	i = 0;
+	while (i < dstsize - 1 && src[i])
 	{
-		i = 0;
-		while (i < dstsize - 1 && src[i])
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
+		dst[i] = src[i];
+		i++;
 	}
+	dst[i] = '\0';
 	return (src_len);
 }
