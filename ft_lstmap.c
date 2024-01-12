f001e419558e510a1e99a162f928a76c2b5a3309
David Wang <devtesting404@gmail.com>
2024-01-12 22:15:22 +0800

    chore: refactor to fix syntax

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -18,19 +18,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*temp;
 
 	head = NULL;
-	if (lst && f)
+	if (!f)
+		return head;
+	while (lst)
 	{
-		while (lst)
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
 		{
-			temp = ft_lstnew(f((*lst).content));
-			if (!temp)
-			{
-				ft_lstclear(&head, del);
-				return (NULL);
-			}
-			ft_lstadd_back(&head, temp);
-			lst = (*lst).next;
+			ft_lstclear(&head, del);
+			return (NULL);
 		}
+		ft_lstadd_back(&head, temp);
+		lst = lst->next;
 	}
 	return (head);
 }
