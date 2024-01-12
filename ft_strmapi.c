902aaf90c40ba54ddd235949f0fae566b6476c41
David Wang <devtesting404@gmail.com>
2024-01-12 23:34:00 +0800

    refactor guard clause

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -17,19 +17,17 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*str;
 	unsigned int	i;
 
-	if (s && f)
-	{
-		str = malloc(sizeof(char) * ft_strlen(s) + 1);
-		if (!str)
-			return (str);
-		i = 0;
-		while (s[i])
-		{
-			str[i] = f(i, s[i]);
-			i++;
-		}
-		str[i] = '\0';
+	if (!s || !f)
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
 		return (str);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
 	}
-	return (NULL);
+	str[i] = '\0';
+	return (str);
 }
